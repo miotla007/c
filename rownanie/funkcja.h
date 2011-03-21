96a487a8ea0e181364574b936dce7326d6d90c2f
Mateusz Motławski <mmotlawski@sigma.inf.ug.edu.pl>
2011-03-21 15:43:03 +0100

    rownanie ax=b

---
+++ rownanie/funkcja.h
@@ -0,0 +1,5 @@
+void wczytaj_dane(void);
+void oblicz_rownanie(void);
+void wypisz_wynik(void);
+
+
---
+++ rownanie/main.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include "funkcja.h"
+
+int main()
+{
+    wczytaj_dane();
+    oblicz_rownanie();
+    wypisz_wynik();
+    return 0;
+}
