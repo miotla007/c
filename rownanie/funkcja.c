a024541e253e330dc7e9bdd3512b92b42a507262
Mateusz Motławski <mmotlawski@sigma.inf.ug.edu.pl>
2011-03-21 16:50:16 +0100

    rownanie ax=b

--- rownanie/funkcja.c
+++ rownanie/funkcja.c
@@ -1,21 +1,21 @@
 #include<stdio.h>
 
-float a,b,x;
+float a, b, x;
 void wczytaj_dane(void)
 {
-printf("Podaj dane rownania a * x = b \n");
-printf("a= ");
-scanf("%f", &a);
-printf("b= ");
-scanf("%f", &b);
+    printf("Podaj dane rownania a * x = b \n");
+    printf("a= ");
+    scanf("%f", &a);
+    printf("b= ");
+    scanf("%f", &b);
 }
 
 void oblicz_rownanie(void)
 {
-x=a/b;
+    x = a / b;
 }
 
 void wypisz_wynik(void)
 {
-printf("Wynikiem rownania %f * x = %f to x = %f \n", a, b, x);
+    printf("Wynikiem rownania %f * x = %f to x = %f \n", a, b, x);
 }
