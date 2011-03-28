90eb36f6be865793a9078259d6f8ef6e8765413e
Mateusz Motławski <mmotlawski@sigma.inf.ug.edu.pl>
2011-03-28 15:52:06 +0200

    liczba max i min

---
+++ petle/zad6/funkcja.h
@@ -0,0 +1,3 @@
+void wczytaj(float tab[6]);
+void sortuj(float tab[6]);
+void wypisz(float tab[6]);
---
+++ petle/zad6/main.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include "funkcja.h"
+int main()
+{
+    float tab[6];
+    wczytaj(tab);
+    sortuj(tab);
+    wypisz(tab);
+    return 0;
+}
